7b4cbfaa07f5e8afa4953e03bb3b7616103d3e2d
Hung Dinh <hdneto17@gmail.com>
2015-06-30 23:25:08 -0400

    some scripts to automate ec2 image

---
+++ ec2/newec2vm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
+my_user_data=`cat $BASEDIR/vm.yml`
+aws ec2 run-instances \
+    --image-id        ami-9eaa1cf6 \
+    --count           1 \
+    --instance-type   t2.micro \
+    --key-name        NYPDVisionZeroAccountability.pem \
+    --security-groups web altssh \
+    --user-data       "$my_user_data"
